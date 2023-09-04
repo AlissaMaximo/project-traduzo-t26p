df4fa132cfa201aa6f29f0eae5238486e07df9e7
Alissa Maximo <alissahiratamaximo@gmail.com>
2023-09-04 06:10:52 -0300

    Req 9 feito

--- tests/controllers/admin/test_admin_controller.py
+++ tests/controllers/admin/test_admin_controller.py
@@ -1,6 +1,29 @@
-# from src.models.history_model import HistoryModel
-# from src.models.user_model import UserModel
+from src.models.history_model import HistoryModel
+from src.models.user_model import UserModel
 
 
 def test_history_delete(app_test):
-    raise NotImplementedError
+    UserModel(
+        {
+            "name": "Mafumafu",
+            "token": "mafuteru",
+        }
+    ).save()
+
+    tl_history = HistoryModel(
+        {
+            "text_to_translate": "Bottle",
+            "translate_from": "en",
+            "translate_to": "pt",
+        }
+    ).save()
+
+    app_test.delete(
+        f"/admin/history/{tl_history.data['_id']}",
+        headers={
+            "Authorization": "mafuteru",
+            "User": "Mafumafu",
+        },
+    )
+
+    assert HistoryModel.find_one({"_id": tl_history.data["_id"]}) is None
