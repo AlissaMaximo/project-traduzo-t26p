e1be274e1e856edd2823437b921d7e7f219913e1
Alissa Maximo <alissahiratamaximo@gmail.com>
2023-09-04 03:42:01 -0300

    Req 4 feito

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
+from models.language_model import LanguageModel
+
 # from deep_translator import GoogleTranslator
-# from models.language_model import LanguageModel
 # from models.history_model import HistoryModel
 
 
@@ -10,7 +11,14 @@ translate_controller = Blueprint("translate_controller", __name__)
 # Reqs. 4 e 5
 @translate_controller.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    return render_template(
+        "index.html",
+        language=LanguageModel.list_dicts(),
+        text_to_translate="O que deseja traduzir",
+        translate_from="pt",
+        translate_to="en",
+        translated="Tradução",
+    )
 
 
 # Req. 6
